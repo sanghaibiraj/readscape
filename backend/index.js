8a7cbe98e8367bf9a348a36e6f4941f3afd2c289
sanghaibiraj <sanghaibiraj@gmail.com>
2024-12-21 21:28:05 +0530

    add book route and book controller

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
 
 const mongoose = require('mongoose');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
+
+const bookRoutes = require('./src/books/book.route');
+
+app.use("/api/books", bookRoutes)
 
 async function main() {
     await mongoose.connect(process.env.DB_URL);
