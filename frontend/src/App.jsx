020a95dbbf75a477c1e55b338bba01c2006373a6
sanghaibiraj <sanghaibiraj@gmail.com>
2024-12-21 11:58:58 +0530

    feat: made login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -10,9 +11,8 @@ function App() {
       <Navbar />
       <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
         <Outlet />
-
       </main>
-      <footer>Footre</footer>
+      <Footer />
     </>
   )
 }
