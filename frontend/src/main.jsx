39a3a6a4917c5cf897bdd4749710e031615902ed
sanghaibiraj <sanghaibiraj@gmail.com>
2024-12-21 14:07:21 +0530

    feat: add checkout page, complete frontend skeleton

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import {Provider} from 'react-redux'
 import { store } from './redux/store.js';
 import 'sweetalert2/dist/sweetalert2.js'
 import CartPage from './pages/books/CartPage.jsx';
+import CheckoutPage from './pages/books/CheckoutPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -24,6 +25,8 @@ createRoot(document.getElementById('root')).render(
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Register />} />
           <Route path='cart' element={<CartPage />} />
+          <Route path='checkout' element={<CheckoutPage />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
