e437484091f2f8e3df76792f7fc47df94749a85e
sanghaibiraj <sanghaibiraj@gmail.com>
2024-12-21 12:42:42 +0530

    feat: made login and register page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 import Home from './pages/home/Home.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,6 +16,8 @@ createRoot(document.getElementById('root')).render(
           <Route index element={<Home />} />
           <Route path="about" element={<h1>About</h1>} />
           <Route path="orders" element={<h1>Orders</h1>} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
